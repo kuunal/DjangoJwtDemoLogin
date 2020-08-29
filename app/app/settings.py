c83104de8a8df83bbd757a0ae1eee94ef109426f
250399 <kunaldeshmukh2503@gmail.com>
2020-08-29 17:09:13 +0530

    [Kunal] ADD: Basic settings for auth and permissions

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES":{
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    },
+    "DEFAULT_PERMISSION_CLASSES":{
+        'rest_framework.permissions.IsAuthenticated',
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
