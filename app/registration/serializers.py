af60f6139e2d40aa5135e8deebf04e640fc33f17
250399 <kunaldeshmukh2503@gmail.com>
2020-08-29 21:02:52 +0530

    [Kunal] ADD: Validation for username

--- app/registration/serializers.py
+++ app/registration/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User
+from app.login_exception import LoginError
+from response_codes import get_response_code
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=255, min_length=8, write_only=True)
@@ -7,4 +9,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username','email','password']
-        
+
+    def validate_username(self, username):
+        if not username.isalnum():
+            raise LoginError(get_response_code('INVALID_USERNAME'))
--- app/response_codes.py
+++ app/response_codes.py
@@ -2,6 +2,7 @@ response = {
     'NULL_USERNAME':{ 'status': 400, 'message':'Username cannot be null'},
     'NULL_EMAIL':{ 'status': 400, 'message':'Username cannot be null'},
     'NULL_PASSWORD':{ 'status': 400, 'message':'Username cannot be null'},
+    'INVALID_USERNAME':{'status':400, 'message':'Username should not contain special charactes and blank spcaes!'}
 }
 
 
