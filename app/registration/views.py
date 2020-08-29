10384bbdc4268315703eb0926154ca7f3e95fc99
250399 <kunaldeshmukh2503@gmail.com>
2020-08-29 20:47:13 +0530

    [Kunal] ADD: Views for registration

--- app/registration/views.py
+++ app/registration/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
+from rest_framework import generics 
+from app.login_exception import LoginError
 # Create your views here.
+
+class RegisterView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if not serializer.is_valid():
+            return LoginError({'status':400,'message':serializer.errors})
+        serializer.save()
+        user_data = serializerdata
+        return Response(user_data, status=status.HTTP_201_CREATED)
