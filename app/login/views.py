a026e997b461a3e94c36e34f5d91f313e0f21810
250399 <kunaldeshmukh2503@gmail.com>
2020-08-30 16:21:38 +0530

    [Kunal] ADD: Views for login

--- app/login/views.py
+++ app/login/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from .serializers import LoginSerializer
+from rest_framework.response import Response
+from rest_framework import generics 
+from app.login_exception import LoginError
+from django.contrib import auth
+from response_codes import get_response_code
 # Create your views here.
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = LoginSerializer(data = request.data)
+        if not serializer.is_valid():
+            raise LoginError(**{'status':400, 'message':serializer.errors})
+        user = auth.authenticate(email=request.data['email'] ,password=request.data['password'] )
+        if not user:
+            raise LoginError(**get_response_code('INVALID_ID_PASS'))
+        response = user.tokens()
+        return Response(response, status=200)    
+        
+        
+         
