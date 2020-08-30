d0298e2f8c701ca525193f965cf5897d3706efb8
250399 <kunaldeshmukh2503@gmail.com>
2020-08-30 16:22:11 +0530

    [Kunal] ADD: Login successfull response code

--- app/response_codes.py
+++ app/response_codes.py
@@ -2,7 +2,10 @@ response = {
     'NULL_USERNAME':{ 'status': 400, 'message':'Username cannot be null'},
     'NULL_EMAIL':{ 'status': 400, 'message':'Username cannot be null'},
     'NULL_PASSWORD':{ 'status': 400, 'message':'Username cannot be null'},
-    'INVALID_USERNAME':{'status':400, 'message':'Username should not contain special charactes and blank spcaes!'}
+    'INVALID_USERNAME':{'status':400, 'message':'Username should not contain special charactes and blank spcaes!'},
+    'INVALID_ID_PASS': {'status': 401, 'message': 'Invalid Id or pass! PLease try Again'},
+    'LOGIN_SUCCESS':{'status': 200, 'message':'Login Successfull'},
+
 }
 
 
