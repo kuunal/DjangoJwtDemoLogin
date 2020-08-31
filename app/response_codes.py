b3e7cf0339fa81208c22020696480106c9eb30df
250399 <kunaldeshmukh2503@gmail.com>
2020-08-31 22:16:11 +0530

    [Kunal] ADD: Response codes related to token

--- app/response_codes.py
+++ app/response_codes.py
@@ -5,7 +5,10 @@ response = {
     'INVALID_USERNAME':{'status':400, 'message':'Username should not contain special charactes and blank spcaes!'},
     'INVALID_ID_PASS': {'status': 401, 'message': 'Invalid Id or pass! PLease try Again'},
     'LOGIN_SUCCESS':{'status': 200, 'message':'Login Successfull'},
-
+    'INVALID_TOKEN':{'status': 401, 'message':'Invalid TOken'},
+    'TOKEN_EXPIRED':{'status': 401, 'message':'TOken expired! Please refresh or login again'},
+    'LOGIN_REQUIRED':{'status': 401, 'message':'You have been logged out! Please login again'},
+    'TOKEN_NOT_FOUND':{'status': 401, 'message':'Please provide token to gain access'},
 }
 
 
