e0c464fcc01c871f697689e4d846cec81b71eb88
250399 <kunaldeshmukh2503@gmail.com>
2020-08-31 16:53:04 +0530

    [Kunal] ADD: login view Post method to generate jwt for user

--- app/login/models.py
+++ app/login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from registration.models import User
 
 # Create your models here.
+class LoginModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    refresh_token = models.CharField(max_length=255)
+
+
--- app/login/views.py
+++ app/login/views.py
@@ -5,6 +5,8 @@ from rest_framework import generics
 from app.login_exception import LoginError
 from django.contrib import auth
 from response_codes import get_response_code
+from .models import LoginModel
+from django.db import connection as cn
 # Create your views here.
 
 class LoginView(generics.GenericAPIView):
@@ -14,10 +16,15 @@ class LoginView(generics.GenericAPIView):
         serializer = LoginSerializer(data = request.data)
         if not serializer.is_valid():
             raise LoginError(**{'status':400, 'message':serializer.errors})
-        user = auth.authenticate(email=request.data['email'] ,password=request.data['password'] )
+        user = auth.authenticate(email=request.data['email'] ,password=request.data['password'])
         if not user:
             raise LoginError(**get_response_code('INVALID_ID_PASS'))
         response = user.tokens()
+        try:
+            cursor = cn.cursor()
+            cursor.execute('insert into login_loginmodel(refresh_token, user_id_id) values(%s, %s)',(response['refresh'], user.id))
+        finally:
+            cursor.close()
         return Response(response, status=200)    
         
         
