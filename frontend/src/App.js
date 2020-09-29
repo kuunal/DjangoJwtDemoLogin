9122a72a929fe82c56faae6a3f8a51456c86749a
250399 <kunaldeshmukh2503@gmail.com>
2020-09-30 01:21:29 +0530

    [Kunal] ADD: Providing store to APP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import Demo from './demo'
-import './App.css';
+import React from "react";
+import Demo from "./demo";
+import "./App.css";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <Demo/>
-      Hello
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Demo />
+        Hello
+      </div>
+    </Provider>
   );
 }
 
