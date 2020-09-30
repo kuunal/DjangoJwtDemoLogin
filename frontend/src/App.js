b23b7f4395fad9cbe35eaa98e2ae2bd6b14bd468
250399 <kunaldeshmukh2503@gmail.com>
2020-09-30 22:31:29 +0530

    [Kunal] ADD: Route for login and produicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,21 @@ import Demo from "./demo";
 import "./App.css";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Demo />
-        Hello
-      </div>
+      <Router>
+        <div className="App" style={container}>
+          <Route path="/login" component={Demo} />
+          <Route path="/products" />
+        </div>
+      </Router>
     </Provider>
   );
 }
-
+const container = {
+  // width: "100%",
+};
 export default App;
