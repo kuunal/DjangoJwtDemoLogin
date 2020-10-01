2d90361a529035bf082db764cc0191be4df1a995
250399 <kunaldeshmukh2503@gmail.com>
2020-10-02 02:54:41 +0530

    [Kunal] REFACTOR: Changed refresh token endpoint to GET

--- app/login/services.py
+++ app/login/services.py
@@ -4,11 +4,13 @@ from app import settings
 from response_codes import get_response_code
 from django.db import connection as cn
 
+
 def refresh_token_required(func):
     def wrapper(request, user=None, *args, **kwargs):
-        token = request.headers.get('refresh-token') 
+        token = request.COOKIES.get('refresh-token')
         if not token:
             raise LoginError(**get_response_code('TOKEN_NOT_FOUND'))
+        print(token)
         try:
             cursor = cn.cursor()
             payload = jwt.decode(token, settings.SECRET_KEY)
@@ -16,10 +18,11 @@ def refresh_token_required(func):
             token_type = payload.get('token_type')
             if token_type != "refresh":
                 raise LoginError(**get_response_code('INVALID_TOKEN'))
-            user_count = cursor.execute('select exists(select id from login_loginmodel where user_id_id = %s)',(user_id,))
+            user_count = cursor.execute(
+                'select exists(select id from login_loginmodel where user_id_id = %s)', (user_id,))
             if not user_count:
                 raise LoginError(**get_response_code('LOGIN_REQUIRED'))
-            return func(request, user=user_id, *args, **kwargs )
+            return func(request, user=user_id, *args, **kwargs)
         except jwt.DecodeError:
             raise LoginError(**get_response_code('INVALID_TOKEN'))
         except jwt.ExpiredSignatureError:
@@ -27,7 +30,3 @@ def refresh_token_required(func):
         finally:
             cursor.close()
     return wrapper
-
-
-
-
--- app/login/views.py
+++ app/login/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .serializers import LoginSerializer, RefreshTokenSerializer
 from rest_framework.response import Response
-from rest_framework import generics 
+from rest_framework import generics
 from app.login_exception import LoginError
 from django.contrib import auth
 from response_codes import get_response_code
@@ -17,15 +17,13 @@ from _datetime import timedelta
 # Create your views here.
 
 
-
 def cookie(func):
     def wrapper(obj, request, *args, **kwargs):
         response = func(obj, request, *args, **kwargs)
-        response.set_cookie(key='refresh-token', value=response.data['refresh'], httponly=True, expires=datetime.datetime.now()+ timedelta(1), path="/")
+        response.set_cookie(key='refresh-token', value=response.data['refresh'],
+                            httponly=True, expires=datetime.datetime.now() + timedelta(1), path="/")
         return response
     return wrapper
-
-
 
 
 class LoginView(generics.GenericAPIView):
@@ -33,31 +31,27 @@ class LoginView(generics.GenericAPIView):
 
     @cookie
     def post(self, request):
-        serializer = LoginSerializer(data = request.data)
+        serializer = LoginSerializer(data=request.data)
         if not serializer.is_valid():
-            raise LoginError(**{'status':400, 'message':serializer.errors})
-        user = auth.authenticate(email=request.data['email'] ,password=request.data['password'])
+            raise LoginError(**{'status': 400, 'message': serializer.errors})
+        user = auth.authenticate(
+            email=request.data['email'], password=request.data['password'])
         if not user:
             raise LoginError(**get_response_code('INVALID_ID_PASS'))
         response = user.tokens()
         try:
             cursor = cn.cursor()
-            cursor.execute('insert into login_loginmodel(refresh_token, user_id_id) values(%s, %s)',(response['refresh'], user.id))
+            cursor.execute(
+                'insert into login_loginmodel(refresh_token, user_id_id) values(%s, %s)', (response['refresh'], user.id))
         finally:
             cursor.close()
         return Response(response, status=200)
 
-         
+
 # @swagger_auto_schema(method='post', request_body=RefreshTokenSerializer)
-# @api_view(('POST',))
+@api_view(('GET',))
 @refresh_token_required
 def custom_token_refresh_view(request, user=None, *args, **kwargs):
-    token = custom_jwt.generate_custom_token(user, settings.JWT_EXPIRATION_TIME, 'access')
-    return Response({'access':token}, status=200)
-        
-
-    
-
-    
-
-    
+    token = custom_jwt.generate_custom_token(
+        user, settings.JWT_EXPIRATION_TIME, 'access')
+    return Response({'access': token}, status=200)
