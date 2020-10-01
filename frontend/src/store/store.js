346d99730b40e0ce3f4d2d3b49275a71fb61d0d5
250399 <kunaldeshmukh2503@gmail.com>
2020-10-02 03:02:47 +0530

    [Kunal] ADD: combineReducers for multiple reducers

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,13 @@
 import thunk from "redux-thunk";
-import { createStore, applyMiddleware } from "redux";
-import { rootreducer } from "../reducers/loginReducers";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { loginReducer } from "../reducers/loginReducers";
+import { tokenReducer } from "../reducers/tokenReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+
+const rootreducer = combineReducers({
+  login: loginReducer,
+  token: tokenReducer,
+});
 
 export const store = createStore(
   rootreducer,
