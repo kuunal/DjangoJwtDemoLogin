acef2eb5b5496bacf58850234f187a977cea7795
kuunal <kunaldeshmukh2503@gmail.com>
2020-10-06 01:02:16 +0530

    [Kunal] ADD: Customized material ui for card

--- frontend/src/components/item.js
+++ frontend/src/components/item.js
@@ -1,28 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import clsx from "clsx";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
 import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { Button } from "@material-ui/core";
+import { Divider } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-  },
   media: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
+    paddingTop: "56.25%",
   },
   expand: {
     transform: "rotate(0deg)",
@@ -35,6 +29,8 @@ const useStyles = makeStyles((theme) => ({
     transform: "rotate(180deg)",
   },
   avatar: {
+    height: 25,
+    width: 25,
     backgroundColor: red[500],
   },
 }));
@@ -49,31 +45,43 @@ export default function Item(props) {
   };
 
   return (
-    <Card className={classes.root}>
+    <Card>
       <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            {author[0]}
-          </Avatar>
+        style={{ textAlign: "left" }}
+        avatar={<Avatar className={classes.avatar}>{author[0]}</Avatar>}
+        title={
+          <Typography style={{ fontSize: "12px" }}>
+            {title.slice(0, 20)}
+          </Typography>
         }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
+        subheader={
+          <Typography style={{ fontSize: "10px" }}>
+            {author.slice(0, 20)}
+          </Typography>
         }
-        title={title}
-        subheader={author}
       />
+      <Divider />
       <CardMedia
         className={classes.media}
         image={image.slice(0, -1)}
-        title="Paella dish"
+        title={description}
       />
+      <Divider />
       <CardContent>
-        <Typography variant="subtitle" color="textPrimary" component="p">
+        <Typography
+          variant="body2"
+          color="textPrimary"
+          style={{ fontSize: "10px", textAlign: "left" }}
+          component="p"
+        >
           PRICE: {price}
         </Typography>
-        <Typography variant="subtitle" color="textPrimary" component="p">
+        <Typography
+          variant="body2"
+          color="textPrimary"
+          component="p"
+          style={{ fontSize: "10px", textAlign: "left" }}
+        >
           QUANTITY: {quantity}
         </Typography>
       </CardContent>
@@ -81,26 +89,11 @@ export default function Item(props) {
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
+        <IconButton aria-label="cart">
+          <ShoppingCartIcon />
         </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
+        <Button variant="contained">Order</Button>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Description:</Typography>
-          <Typography paragraph>{description}</Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 }
