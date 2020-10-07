f593acbfa8d4e5f2129fe870d7e817877962c9ed
kuunal <kunaldeshmukh2503@gmail.com>
2020-10-07 22:39:29 +0530

    [Kunal] ADD: Basic structure for pagination

--- frontend/src/components/products.js
+++ frontend/src/components/products.js
@@ -5,15 +5,15 @@ import axios from "axios";
 import Headers from "./headers";
 import { Grid, makeStyles } from "@material-ui/core";
 import Item from "./item.js";
+import { Pagination } from "@material-ui/lab";
 
 const refreshTokenURI = process.env.REACT_APP_BACKENDURI + "login/refresh/";
 const useStyle = makeStyles({
   gridContainer: {
-    paddingLeft: "100px",
-    paddingRight: "100px",
+    paddingLeft: "150px",
+    paddingRight: "150px",
   },
 });
-
 const gridStyle = {
   width: 230,
   height: 300,
@@ -23,13 +23,17 @@ const gridStyle = {
 function ProductsComponent({ loginToken, newAccessToken, requestToken }) {
   const [products, setProducts] = useState([]);
   const [token, setToken] = useState(null);
-  const productURI = process.env.REACT_APP_BACKENDURI + "products/?pageno=1";
+  const [pageNo, setPageNo] = useState("1");
+  const productURI =
+    process.env.REACT_APP_BACKENDURI + "products/?pageno=" + pageNo;
+  const [nextPage, setNextPage] = useState("");
+  const [prevPage, setPrevPage] = useState("");
 
   const classes = useStyle();
   useEffect(() => {
     if (newAccessToken === "") requestToken(refreshTokenURI);
     else getProducts();
-  }, [newAccessToken]);
+  }, [newAccessToken, pageNo]);
 
   const getProducts = () => {
     axios
@@ -38,15 +42,23 @@ function ProductsComponent({ loginToken, newAccessToken, requestToken }) {
         { headers: { "x-token": newAccessToken } },
         { withCredentials: true }
       )
-      .then((res) => setProducts([...products, ...res.data]))
+      .then((res) => {
+        setProducts([...res.data.products]);
+        setNextPage(res.data.next_page);
+        setPrevPage(res.data.prev_page);
+      })
       .catch((res) => console.log(res.response));
   };
+
+  const handleChange = (event, value) => {
+    setPageNo(value);
+  };
+
   return (
     <Grid container direction="column">
       <Grid item>
         <Headers />
       </Grid>
-      {console.log(products.len)}
       <Grid
         item
         container
@@ -54,13 +66,34 @@ function ProductsComponent({ loginToken, newAccessToken, requestToken }) {
         spacing="4"
         className={classes.gridContainer}
       >
+        <Grid item container>
+          <Grid item container justify="space-between">
+            Book
+            <Grid item>
+              <select>
+                <option>Relevance</option>
+                <option>Price</option>
+                <option>Author</option>
+                <option>Title</option>
+              </select>
+            </Grid>
+          </Grid>
+        </Grid>
         {products.map((item, index) => (
           <Grid item style={gridStyle}>
             <Item item={item} key={index} />
           </Grid>
         ))}
+        <Grid item container justify="center">
+          <Pagination
+            color="primary"
+            count={7}
+            siblingCount={0}
+            boundaryCount={1}
+            onChange={handleChange}
+          />
+        </Grid>
       </Grid>
-      <Grid item>Pagination</Grid>
     </Grid>
   );
 }
