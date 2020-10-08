975d53a4fbfce891e22e6c3c4bb751616185006e
kuunal <kunaldeshmukh2503@gmail.com>
2020-10-09 01:36:59 +0530

    [Kunal] ADD: New file for wishlist icon in header

---
+++ frontend/src/components/headers/wishListIcon.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import { IconButton } from "@material-ui/core";
+
+export default function wishListIcon() {
+  return (
+    <IconButton>
+      <FavoriteIcon />
+    </IconButton>
+  );
+}
