5bc8c7a2f633eedb0ecb9b4fd0ccea88ef0231f8
kuunal <kunaldeshmukh2503@gmail.com>
2020-10-09 01:36:44 +0530

    [Kunal] ADD: New file for userprofile icon in header

---
+++ frontend/src/components/headers/userProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { IconButton } from "@material-ui/core";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+
+export default function userProfile() {
+  return (
+    <IconButton>
+      <AccountCircleIcon />
+    </IconButton>
+  );
+}
