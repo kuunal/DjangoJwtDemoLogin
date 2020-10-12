b1c3334e3d02005873701cae6fc0d7e79025d10e
kuunal <kunaldeshmukh2503@gmail.com>
2020-10-12 10:59:09 +0530

    [Kunal] REFACTOR: HTLM Input

--- frontend/src/components/headers/searchBar.js
+++ frontend/src/components/headers/searchBar.js
@@ -3,13 +3,20 @@ import TextField from "@material-ui/core/TextField";
 import SearchIcon from "@material-ui/icons/Search";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import Typed from "react-typed";
+import { Input } from "@material-ui/core";
 
 export default function SearchBar() {
   return (
     <TextField
-      style={{ background: "white", borderRadius: "3px", width: "100%" }}
+      style={{
+        background: "white",
+        borderRadius: "3px",
+        width: "100%",
+        padding: "2px",
+      }}
       variant="standard"
       id="input-with-icon-textfield"
+      placeholder="search"
       InputProps={{
         endAdornment: (
           <InputAdornment>
@@ -17,6 +24,23 @@ export default function SearchBar() {
           </InputAdornment>
         ),
       }}
+      // />
+      // <Typed
+      //   strings={["Three mistakes of my life", "Rich dad poor dad", ""]}
+      //   backSpeed={20}
+      //   typeSpeed={30}
+      // >
+      //   <input
+      //     type="text"
+      //     style={{
+      //       background: "white",
+      //       borderRadius: "3px",
+      //       width: "100%",
+      //       height: "80%",
+      //       border: "none",
+      //     }}
+      //     placeholder="search"
+      // </Typed>
     />
   );
 }
