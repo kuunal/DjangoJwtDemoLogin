2bee61a0ad5aee713a7074519e449d7b1eb6b905
kuunal <kunaldeshmukh2503@gmail.com>
2020-10-12 10:56:59 +0530

    [KUNAL] REFACTOR: Query for Pagination

--- app/product/models.py
+++ app/product/models.py
@@ -4,13 +4,15 @@ import math
 
 class ProductManager():
     @staticmethod
-    def all(page):
+    def all(page, sortby):
         PAGINATOR_ITEMS = 8
         try:
             cursor = connection.cursor()
             cursor.execute(
-                'select id, author, title, image, quantity, price, description, (select count(id) from product) as length from product where id > %s limit %s', ((int(page)*PAGINATOR_ITEMS)-PAGINATOR_ITEMS, PAGINATOR_ITEMS))
+                f'select SQL_CALC_FOUND_ROWS id, author, title, image, quantity, price, description from product order by {sortby} limit %s,%s;', ((int(page)*PAGINATOR_ITEMS)-PAGINATOR_ITEMS, PAGINATOR_ITEMS))
             rows = cursor.fetchall()
+            cursor.execute("select found_rows()")
+            total_products = cursor.fetchone()
             objects = []
             if rows:
                 for row in rows:
@@ -22,9 +24,8 @@ class ProductManager():
                     product_object.quantity = row[4]
                     product_object.price = row[5]
                     product_object.description = row[6]
-                    product_object.total_products = row[7]
                     objects.append(product_object)
-            return objects
+            return objects, total_products
         finally:
             cursor.close()
 
@@ -40,4 +41,6 @@ class Product:
         self.price = None
         self.quantity = None
         self.description = None
-        self.total_products = None
+
+
+# 'select id, author, title, image, quantity, price, description, (select count(id) from product) as length from product where id > %s order by %s limit %s', ((int(page)*PAGINATOR_ITEMS)-PAGINATOR_ITEMS, sortby, PAGINATOR_ITEMS))select id, author, title, image, quantity, price, description from product where %s > %s order by %s limit %s;"
