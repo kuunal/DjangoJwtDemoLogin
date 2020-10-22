7f1b1735c3444749a70a7bff931cda2d4bf556be
kuunal <kunaldeshmukh2503@gmail.com>
2020-10-23 03:11:26 +0530

    [Kunal] ADD: Url for product search

--- app/product/urls.py
+++ app/product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_products
+from .views import get_products, type_ahead_search
 
-urlpatterns=[
-    path('', get_products)
+urlpatterns = [
+    path('', get_products),
+    path('search', type_ahead_search)
 ]
